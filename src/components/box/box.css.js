849f8e711060ddf9e3b3aa37e03e7b3c3d659ef2
billpope <epop.bill@gmail.com>
2019-12-11 09:24:49 -0500

    bg img

--- src/components/box/box.css.js
+++ src/components/box/box.css.js
@@ -4,5 +4,5 @@ export const Container = styled.div`
   padding: 2rem 4rem;
   max-width: 100%;
   background: white url(${props => props.inputColor || ''}) no-repeat fixed
-    center 70px;
+    center 20%;
 `;
